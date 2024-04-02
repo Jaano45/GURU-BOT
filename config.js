db3e16790453176255e6e4d8dd9ceb37f7c0c117
Jaano45 <165716813+Jaano45@users.noreply.github.com>
2024-04-02 12:36:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011', 'Syco', true],
+  ['6281255369012', '302', true], 
   [''] 
 ] //Number of owners
 
